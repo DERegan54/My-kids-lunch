fff85f2f321442d967b1db93f47822a5ad8332f1
Danielle Regan Arnett <regandanielle54@gmail.com>
2023-06-30 05:44:51 -0600

    added nutrition info to foodCard

--- backend/mkl_db-seed.sql
+++ backend/mkl_db-seed.sql
@@ -11,20 +11,20 @@ VALUES ('Ham Sandwich', 'ham and cheese sandwich on wheat bread with an apple, b
        ('PBJ', 'Peanut butter and grape jelly on sourdough bread with a clementine, celery sticks, string cheese, and whole milk', 'peanut butter', 'sourdough bread', 'clementine', 'celery', 'string cheese', 'grape jelly', 'whole milk');
 
 INSERT INTO foods (title, category, serving_size, calories, fat, protein, carbohydrates, sugar, lunch_id)
-VALUES ('ham', 'protein', '3 oz', 180, 5, 0.3, 0.5, 21, 1),
-       ('wheat bread', 'carb', '2 slices', 240, 2, 2, 30, 2, 1),
-       ('apple', 'fruit', '1 apple', 59, 0, 0, 14, 11, 1),
-       ('baby carrots', 'vegetable', '10 items', 30, 0, 0, 8.3, 4.8, 1),
-       ('american cheese', 'fat', '1 slice', 80, 4, 2, 8.8, 6.2, 1),
-       ('oreos', 'sweet', '3 cookies', 330, 8, 3, 41, 45, 1),
-       ('gatorade', 'beverage', '12 oz', 105, 0, 0, 6.4, 5.3, 1),
-       ('peanut butter', 'protein', '2 tbsp', 260, 16, 14, 12, 3, 2),
-       ('rye bread', 'carb', '2 slices', 240, 2, 2, 24, 2, 2),
-       ('clementine', 'fruit', '2 clementines', 80, 0, 0, 11, 8, 2),
-       ('celery', 'vegetable', '5 sticks', 20, 0, 0, 1, 0.2, 2),
-       ('string cheese', 'fat', '1 piece',  70, 2, 2, 2, 1, 2),
-       ('grape jelly', 'sweet', '1 tbsp', 85, 1, 0.5, 21, 18, 2),
-       ('whole milk', 'beverage', '8 oz', 100, 8, 10, 8, 10, 2);
+VALUES ('Ham', 'protein', '3 oz', 180, 5, 0.3, 0.5, 21, 1),
+       ('Wheat Bread', 'carb', '2 slices', 240, 2, 2, 30, 2, 1),
+       ('Honecrisp Apple', 'fruit', '1 apple', 59, 0, 0, 14, 11, 1),
+       ('Baby Carrots', 'vegetable', '10 items', 30, 0, 0, 8.3, 4.8, 1),
+       ('American Cheese', 'fat', '1 slice', 80, 4, 2, 8.8, 6.2, 1),
+       ('Oreo Cookies', 'sweet', '3 cookies', 330, 8, 3, 41, 45, 1),
+       ('Gatorade', 'beverage', '12 oz', 105, 0, 0, 6.4, 5.3, 1),
+       ('Peanut Butter', 'protein', '2 tbsp', 260, 16, 14, 12, 3, 2),
+       ('Sourdough bread', 'carb', '2 slices', 240, 2, 2, 24, 2, 2),
+       ('Clementine', 'fruit', '2 clementines', 80, 0, 0, 11, 8, 2),
+       ('Celery Sticks', 'vegetable', '5 sticks', 20, 0, 0, 1, 0.2, 2),
+       ('String Cheese', 'fat', '1 piece',  70, 2, 2, 2, 1, 2),
+       ('Grape Jelly', 'sweet', '1 tbsp', 85, 1, 0.5, 21, 18, 2),
+       ('Whole Milk', 'beverage', '8 oz', 100, 8, 10, 8, 10, 2);
        
 
 INSERT INTO reviews (review_text, user_id, lunch_id)
